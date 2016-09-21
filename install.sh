9cf25cd4db2efe41b9911abb23a1fbd0df2e90bf
sylverebastien <sylvere.bastien@gmail.com>
2016-09-21 19:01:00 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+sudo apt-get install -y apache2
+sudo apt-get install -y php5 libapache2-mod-php5
+sudo apt-get install -y curl php5-curl
+sudo apt-get install -y alsa-utils mpg123 moc moc-ffmpeg-plugin 
+sudo apt-get install -y wakeonlan
+
+cd ~/Downloads/
+git clone git://git.drogon.net/wiringPi
+git clone https://github.com/r10r/rcswitch-pi
+git clone git://github.com/ninjablocks/433Utils.git
+
+cd ~/Downloads/wiringPi/
+./build
+sudo ldconfig
+
+cp ~/Downloads/rcswitch-pi/* ~/Downloads/433Utils/rc-switch/
+cd ~/Downloads/433Utils/RPi_utils/
+make
+sudo ln -s ~/Downloads/433Utils/RPi_utils/codesend /usr/bin/codesend
+rm -rf ~/Downloads/rcswitch-pi
