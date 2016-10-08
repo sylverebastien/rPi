51cb26f72cbe94edd583c5794258c980866b7c4b
Sylvere Bastien <sylvere.bastien@gmail.com>
2016-10-08 15:54:10 +0200

    gmail_fetch: focus on unseen mails + fixed multipart mail issues

--- gmail_fetch.py
+++ gmail_fetch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Very simple Python script to dump all emails in an IMAP folder to files.  
+# Very simple Python script to dump all emails in an IMAP folder to files.
 # This code is released into the public domain.
 #
 # RKI Nov 2013
@@ -24,29 +24,32 @@ def process_mailbox(M):
     """
     Dump all emails in the folder to files in output directory.
     """
-
-    rv, data = M.search(None, "ALL")
-    if rv != 'OK':
+    rv, data = M.search(None, '(UNSEEN)')
+    if data == ['']:
         print "No messages found!"
         return
 
     for num in data[0].split():
         rv, data = M.fetch(num, '(RFC822)')
-        if rv != 'OK':
-            print "ERROR getting message", num
-            return
-        print "Writing message ", num
-	msg = email.message_from_string(data[0][1])
-	messageMainType = msg.get_content_maintype()
-        if messageMainType == 'text':
-        	body = msg.get_payload()
-
+        print "Writing message :", num
+        msg = email.message_from_string(data[0][1])
+        rv, data = M.store(num,'+FLAGS','\\Seen')
+        if msg.is_multipart():
+            for part in msg.walk():
+                ctype = part.get_content_type()
+                cdispo = str(part.get('Content-Disposition'))
+                if ctype == 'text/plain' and 'attachment' not in cdispo:
+                    body = part.get_payload(decode=True)
+                    break
+        else:
+            body = msg.get_payload(decode=True)
         f = open('%s/%s.eml' %(OUTPUT_DIRECTORY, num), 'wb')
         f.write(unicode(email.header.decode_header(msg['From'])[0][0])+'\n')
         f.write(msg['Date']+'\n')
         f.write(unicode(email.header.decode_header(msg['Subject'])[0][0])+'\n')
-	f.write(body)
+        f.write(body)
         f.close()
+
 
 def main():
     M = imaplib.IMAP4_SSL(IMAP_SERVER)
