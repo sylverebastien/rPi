82895f3d33c746128be058b60698beb3e18160f9
sylverebastien <sylvere.bastien@gmail.com>
2016-10-12 19:24:49 +0200

    app/Models/Gladys & infos: updated code

--- domotique/app/Models/GladysModel.php
+++ domotique/app/Models/GladysModel.php
@@ -11,17 +11,14 @@ class GladysModel extends AppModel {
 		$data['main']['temp']= explode(".", $data['main']['temp']);
 		$data['main']['humidity']= explode(".", $data['main']['humidity']);
 		$data['wind']['speed']= explode(".", $data['wind']['speed']);
-
 		// Taduction météo
 		$mot = array("Clear","Clouds","Rain");
 		$mot2 = array("Clair","nuageux","Pluvieux");
 		$data['weather'][0]['main'] = str_replace($mot, $mot2, $data['weather'][0]['main']);
-
 		// Phrases pour TTS
 		$text ='Il fait '.$data['main']['temp'][0].' degrés, et le temps est '.$data['weather'][0]['main'].'.';
 		$text2 ='Il y a '.$data['main']['humidity'][0].' % d\'humidité et le vent souffle à '.$data['wind']['speed'][0].' mètres par seconde.';
 		$weekend = strftime('%A');
-
 		// TTS
 		$this->direPhrase($text);
 		$this->direPhrase($text2);
@@ -41,6 +38,7 @@ class GladysModel extends AppModel {
 		exec('sudo omxplayer -o local temp.wav');
 		exec('rm temp.wav');
 	}
+	
 	public function find($name) {
 		foreach ($this->globals as $global_name => $global) {
 			if ($global_name == $name) {
@@ -50,15 +48,18 @@ class GladysModel extends AppModel {
 			}
 		}
 	}
+	
 	public function toggle($val){
 		$this->statut = $val;
 		/* Action for either reveil or chauffage */
 		$this->update();
 	}
+	
 	public function update() {
 		$this->datas['globals'][$this->name]['statut'] = $this->statut;
 		parent::save();
 	}
+	
 	public function getCurrentState() {
 		$this->Prise = new GladysModel();
 		$states = [];
@@ -70,15 +71,12 @@ class GladysModel extends AppModel {
 		$states['alarme'] = ($states['alarme']['statut'] == 1) ? 'checked' : '';
 		return $states;
 	}
+	
 	public function respond($tothisphrase) {
 		$tothisphrase = ucfirst($tothisphrase);
 
 		if ( stripos($tothisphrase, 'Dis') !== false && stripos($tothisphrase, 'dis') == 0 ) {
 			$phraseadire = str_replace('Dis', '', $tothisphrase);
-			$tothisphrase = 'Dis';
-		}
-		if ( stripos($tothisphrase, 'Dit') !== false && stripos($tothisphrase, 'dis') == 0 ) {
-			$phraseadire = str_replace('Dit', '', $tothisphrase);
 			$tothisphrase = 'Dis';
 		}
 		if ( stripos($tothisphrase, 'Exec') !== false && stripos($tothisphrase, 'Exec') == 0 ) {
@@ -93,12 +91,12 @@ class GladysModel extends AppModel {
 
 			case 'Radio':
 			exec('sudo pkill mpg321');
-			$this->direPhrase('Novaa, Brume, Jazz, France inter, Canuut. Radio p pour arrêter.');
+			$this->direPhrase('Nova, Brume, Jazz, France inter, Canut. Radio p pour arrêter.');
 			break;
 
 			case 'Nova':
 			exec('sudo pkill mpg321');
-			$this->direPhrase('Lancement de Radio Novaa.');
+			$this->direPhrase('Lancement de Radio Nova.');
 			sleep(1);
 			exec('sudo mpg321 "http://novazz.ice.infomaniak.ch/novazz-128"');
 			break;
@@ -119,14 +117,14 @@ class GladysModel extends AppModel {
 
 			case 'France inter':
 			exec('sudo pkill mpg321');
-			$this->direPhrase('Lancement de france inter.');
+			$this->direPhrase('Lancement de France Inter.');
 			sleep(1);
 			exec('sudo mpg321 "http://audio.scdn.arkena.com/11008/franceinter-midfi128.mp3"');
 			break;
 
 			case 'Canut':
 			exec('sudo pkill mpg321');
-			$this->direPhrase('Lancement de Radio canuut.');
+			$this->direPhrase('Lancement de Radio Canut.');
 			sleep(1);
 			exec('sudo mpg321 "http://live.francra.org:8000/radiocanut?.mp3"');
 			break;
@@ -136,9 +134,9 @@ class GladysModel extends AppModel {
 			break;
 
 			case 'Radio p':
+			$this->Music = new MusicModel();
+			$this->Music->pause();
 			exec('sudo pkill mpg321');
-			sleep(1);
-			$this->direPhrase('Arrêt de la radio.');
 			break;
 
 
