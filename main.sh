997fb08a455d60b8453028eace41397149d46718
sylverebastien <sylvere.bastien@gmail.com>
2016-11-26 18:46:58 +0100

    main: refactored installation code

--- main.sh
+++ main.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-flags='hiu'
+flags='hi:u'
 _help () {
 cat <<EOF
 Usage: ${0##*/} [-$flags]
 -h  display this help
--i  install
+-i  install [all/base/mail/voice/camera/radiofz]
 -u  update domotique folder in apache
 EOF
 }
 
 _install () {
-  sudo apt-get update
-  sudo apt-get -y upgrade
-  sudo apt-get -y dist-upgrade
-  sudo apt-get -y autoremove
-
-  sudo apt-get install -y bc
-
-  sudo apt-get install -y apache2
-  sudo apt-get install -y php5 libapache2-mod-php5
-  sudo apt-get install -y curl php5-curl
-  sudo apt-get install -y alsa-utils mpg123 mpg321 moc moc-ffmpeg-plugin
-  sudo apt-get install -y wakeonlan
-
-  sudo apt-get install -y ssmtp mailutils mpack
-  sudo chfn -f "rPi" root
-
-  sudo apt-get install -y libttspico-utils
-
-  sudo apt-get install motion
-
-  mkdir -p $DIR/Downloads
-  cd $DIR/Downloads/
-  git clone git://git.drogon.net/wiringPi
-  git clone https://github.com/r10r/rcswitch-pi
-  git clone git://github.com/ninjablocks/433Utils.git
-
-  cd wiringPi/
-  ./build
-  sudo ldconfig
-
-  cp $DIR/Downloads/rcswitch-pi/* $DIR/Downloads/433Utils/rc-switch/
-  cd $DIR/Downloads/433Utils/RPi_utils/
-  make
-  sudo ln -s $DIR/Downloads/433Utils/RPi_utils/codesend /usr/bin/codesend
-  rm -rf $DIR/Downloads/rcswitch-pi
-
-  sudo service apache2 restart
+  if [[ $1 == "all" || $1 == "base" ]]; then
+	sudo apt-get update
+	sudo apt-get install -y bc
+	sudo apt-get install -y apache2
+	sudo apt-get install -y php5 libapache2-mod-php5
+	sudo apt-get install -y curl php5-curl
+	sudo apt-get install -y alsa-utils mpg123 mpg321 moc moc-ffmpeg-plugin
+	sudo apt-get install -y wakeonlan
+  elif [[ $1 == "all" || $1 == "mail" ]]; then
+	sudo apt-get update
+	sudo apt-get install -y ssmtp mailutils mpack
+	sudo chfn -f "rPi" root
+	mkdir -p $DIR/mail
+  elif [[ $1 == "all" || $1 == "voice" ]]; then
+	sudo apt-get install -y libttspico-utils
+  elif [[ $1 == "all" || $1 == "camera" ]]; then
+	sudo apt-get install motion
+	mkdir -p $DIR/motion
+  elif [[ $1 == "all" || $1 == "radiofz" ]]; then
+	mkdir -p $DIR/utils
+	cd $DIR/utils
+	git clone git://git.drogon.net/wiringPi
+	git clone https://github.com/r10r/rcswitch-pi
+	git clone git://github.com/ninjablocks/433Utils.git
+	cd wiringPi/
+	./build
+	sudo ldconfig
+	cp $DIR/utils/rcswitch-pi/* $DIR/utils/433Utils/rc-switch/
+	cd $DIR/utils/433Utils/RPi_utils/
+	make
+	sudo ln -s $DIR/utils/433Utils/RPi_utils/codesend /usr/bin/codesend
+	sudo ln -s $DIR/utils/433Utils/RPi_utils/RFSniffer /usr/bin/RFSniffer
+	rm -rf $DIR/utils/rcswitch-pi
+  else
+	sudo service apache2 restart
+  fi
 }
 
 _update () {
@@ -63,7 +62,7 @@ while getopts ":$flags" o; do
   case "${o}" in
     h)  _help
     exit;;
-    i)  _wizard
+    i)  _install "${OPTARG}"
     exit;;
     u)  _update
     exit;;
