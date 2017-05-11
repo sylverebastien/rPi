ae1b8c4609e6622a533e5de891f20796267de2e8
Sylvere <sylvere@sylvere-VM>
2017-05-11 21:19:41 +0200

    main: fixed snowboy installation
    moved resources into utils/resources
    added common.res into utils/resources/
    added .gitignore
    updated utils/snowboydetect script

--- main.sh
+++ main.sh
@@ -17,51 +17,57 @@ _install () {
 	sudo apt-get install -y apache2
 	sudo apt-get install -y php5 libapache2-mod-php5
 	sudo apt-get install -y curl php5-curl
-	sudo apt-get install -y alsa-utils mpg123 mpg321 moc moc-ffmpeg-plugin
+	sudo apt-get install -y alsa-utils mpg123 mpg321 moc moc-ffmpeg-plugin omxplayer
 	sudo apt-get install -y wakeonlan
+	sudo modprobe snd_bcm2835
+	sudo amixer -c 0 cset numid=3 1
   fi
   if [[ $1 == "all" || $1 == "mail" ]]; then
 	sudo apt-get install -y ssmtp mailutils mpack
 	sudo chfn -f "rPi" root
-	mkdir -p $DIR/mail
+	mkdir -p $DIR/utils/mail
   fi
   if [[ $1 == "all" || $1 == "voice" ]]; then
 	sudo apt-get install -y libttspico-utils
   fi
   if [[ $1 == "all" || $1 == "camera" ]]; then
 	sudo apt-get install -y motion
-	mkdir -p $DIR/motion
+	sudo modprobe bcm2835-v4l2
+	echo "bcm2835-v4l2" | sudo tee -a /etc/modules
+	mkdir -p $DIR/utils/motion
   fi
   if [[ $1 == "all" || $1 == "radiofz" ]]; then
 	mkdir -p $DIR/utils
 	cd $DIR/utils
-	git clone git://git.drogon.net/wiringPi
-	git clone https://github.com/r10r/rcswitch-pi
-	git clone git://github.com/ninjablocks/433Utils.git
+	git clone https://github.com/wiringPi/wiringPi.git
+	git clone --recursive https://github.com/ninjablocks/433Utils.git
 	cd wiringPi/
 	./build
 	sudo ldconfig
-	cp $DIR/utils/rcswitch-pi/* $DIR/utils/433Utils/rc-switch/
 	cd $DIR/utils/433Utils/RPi_utils/
 	make
 	sudo ln -s $DIR/utils/433Utils/RPi_utils/codesend /usr/bin/codesend
 	sudo ln -s $DIR/utils/433Utils/RPi_utils/RFSniffer /usr/bin/RFSniffer
-	rm -rf $DIR/utils/rcswitch-pi
   fi
   if [[ $1 == "all" || $1 == "sensor" ]]; then
-	cc -Wall utils/dht.c -o utils/dht -lwiringPi
+	sudo gcc -Wall $DIR/utils/dht.c -o $DIR/utils/dht -lwiringPi -lcrypt -lpthread -lm -lrt
 	sudo ln -s $DIR/utils/dht /usr/bin/dht
   fi
   if [[ $1 == "all" || $1 == "snowboy" ]]; then
-	arecord -l
-	sudo apt-get install -y python-pip
+	sudo apt-get install -y python-dev python-pip
 	sudo pip install --upgrade pip
-	aplay -l
-	##nano ~/.asoundrc
 	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 	sudo apt-get install -y python-pyaudio python3-pyaudio sox
 	sudo apt-get install -y libatlas-base-dev libatlas-dev
 	sudo pip install pyaudio
+	sudo apt-get install -y swig
+	cd $DIR/utils
+	git clone https://github.com/Kitt-AI/snowboy.git
+	cd snowboy/swig/Python && make && cd $DIR/utils
+	cp snowboy/swig/Python/_snowboydetect.so .
+	cp snowboy/examples/Python/snowboydecoder.py .
+	sudo chmod u+x snowboyde*
+	rm -rf snowboy/
   fi
   sudo service apache2 restart
 }
